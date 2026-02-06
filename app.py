373d114e0f0c1151b904c2c52b1e7a0a649f18f6
AHDanielSharon <ahdanielsharon@gmail.com>
2026-02-06 19:35:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 client = OpenAI(
     base_url="https://router.huggingface.co/v1",
-    api_key=""
+    api_key="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"
 )
 st.set_page_config(
     page_title="AI Study Buddy",
